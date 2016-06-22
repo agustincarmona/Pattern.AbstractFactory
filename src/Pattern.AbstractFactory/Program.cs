dda05e84331d4eeff877021dd7bed9a9b173a086
agustincarmona <agustin.carmona@gmail.com>
2016-06-22 16:29:46 +0200

    Abstract factory pattern with interfaces for replace abstract classes

--- src/Pattern.AbstractFactory/Program.cs
+++ src/Pattern.AbstractFactory/Program.cs
@@ -9,6 +9,71 @@ namespace Pattern.AbstractFactory
     {
         public static void Main(string[] args)
         {
+            FordCar carFord = (FordCar)new CarFactory(CarBrand.Ford).Create();
+            Console.WriteLine($"Created car {carFord.GetBrand()}");
+
+            SeatCar carSeat = (SeatCar)new CarFactory(CarBrand.Seat).Create();
+            Console.WriteLine($"Created car {carSeat.GetBrand()}");
+
+            Console.ReadKey();
         }
     }
+
+    interface IVehicle
+    {
+        string GetBrand();
+    }
+
+    interface ICar : IVehicle
+    {
+    }
+
+    interface IVehicleFactory
+    {
+        IVehicle Create();
+    }
+
+    class CarFactory : IVehicleFactory
+    {
+        private CarBrand Brand;
+
+        Dictionary<CarBrand, Type> brandMap = new Dictionary<CarBrand, Type>
+        {
+            { CarBrand.Ford, typeof(FordCar) },
+            { CarBrand.Seat, typeof(SeatCar) }
+        };
+        
+        public CarFactory(CarBrand brand)
+        {
+            this.Brand = brand;
+        }
+
+        public IVehicle Create()
+        {
+            return (ICar)Activator.CreateInstance(this.brandMap[this.Brand]);
+        }
+    }
+
+    class FordCar : ICar
+    {
+        public string GetBrand()
+        {
+            return "Ford";
+        }
+    }
+
+    class SeatCar : ICar
+    {
+        public string GetBrand()
+        {
+            return "Seat";
+        }
+    }
+
+    enum CarBrand
+    {
+        Ford,
+        Seat
+    }
+
 }
